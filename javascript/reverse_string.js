e5d7c4f06166ce611c938d53b0b5a5f755442603
Tasreen Mowreen <tmowreen97@gmail.com>
2022-04-30 15:24:31 -0400

    Done.

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,9 +1,13 @@
 function reverseString(str) {
   // type your code here
+  return str.split('').reverse().join('')
 }
 
 if (require.main === module) {
   // add your own tests in here
+  console.log("Expecting: 'eurt'");
+  console.log("=>", reverseString("true"))
+
   console.log("Expecting: 'ih'");
   console.log("=>", reverseString("hi"));
 
@@ -16,4 +20,7 @@ if (require.main === module) {
 module.exports = reverseString;
 
 // Please add your pseudocode to this file
+
 // And a written explanation of your solution
+  // using .reverse() on an array will help immensly.
+  //in order to use that, we first have to transform the string into an array, reverse, and then turn it back into a string.
